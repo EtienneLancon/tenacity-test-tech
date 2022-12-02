575bfc3d3a1733b5d9f0e2afb98271054382b199
etienne <lancon.etienne@gmail.com>
2022-12-02 11:15:28 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <!--optionnalcomponentwellsee-->>
+    </div>
+  </header>
+
+  <main>
+    <!--maincomponent-->
+  </main>
+</template>
+
+<style scoped>
+
+</style>
